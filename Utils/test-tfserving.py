0d1ac4851b5aac82b4f8869a7d3481f6cea3bf54
Mirko Rodriguez <mirko.rodriguezm@gmail.com>
2020-07-21 14:53:22 -0500

    Adding script for testing the model deployed on TensorServing.

--- Utils/test-tfserving.py
+++ Utils/test-tfserving.py
@@ -6,8 +6,8 @@ from tensorflow.keras.preprocessing import image
 
 # Argumentos
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="Image path is needed.")
-ap.add_argument("-m", "--model", required=True, help="Model name is needed.")
+ap.add_argument("-i", "--image", required=True, help="Image PATH is needed.")
+ap.add_argument("-m", "--model", required=True, help="Model NAME is needed.")
 ap.add_argument("-p", "--port", required=True, help="Model PORT number is needed.")
 args = vars(ap.parse_args())
 
@@ -20,24 +20,27 @@ print("Image:",image_path)
 print("Puerto:",port)
 
 
-# Preprocesar imagen
-image = image.img_to_array(image.load_img(image_path, target_size=(224, 224)))
+# Image processing
+test_image = image.load_img(image_path,target_size = (224, 224))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 test_image = test_image.astype('float32')
 test_image /= 255.0
-payload = {"instances": [{'input_image': img.tolist()}]}
 
-# URI
+data = json.dumps({"signature_name": "serving_default", "instances": test_image.tolist()})
+headers = {"content-type": "application/json"}
 uri = ''.join(['http://127.0.0.1:',port,'/v1/models/',model_name,':predict'])
 print("URI:",uri)
 
-# Request al modelo desplegado en TensorFlow Serving
-r = requests.post(uri, json=payload)
-pred = json.loads(r.content.decode('utf-8'))
+json_response = requests.post(uri, data=data, headers=headers)
+predictions = json.loads(json_response.text)['predictions'][0]
+print(predictions)
+index = np.argmax(predictions)
 
-# Decodificando decoder util
-predictions = decode_predictions(np.array(pred['predictions']),top=1)
-print("Predictions:\n",predictions)
-print("Class:",predictions[0][0][1])
-print("Score",predictions[0][0][2])
+CLASSES = ['Daisy', 'Dandelion', 'Rosa', 'Girasol', 'Tulipán']
+ClassPred = CLASSES[index]
+ClassProb = predictions[index]
+
+print("Index:", index)
+print("Pedicción:", ClassPred)
+print("Prob:", ClassProb)
