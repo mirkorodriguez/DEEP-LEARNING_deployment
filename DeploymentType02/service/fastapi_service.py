2798cb7de2a75c9b934a285b9b1b255a787a66f5
Mirko Rodriguez <mirko.rodriguezm@gmail.com>
2020-07-23 07:19:32 -0500

    Creating FastAPI service script.

--- DeploymentType02/service/fastapi_service.py
+++ DeploymentType02/service/fastapi_service.py
@@ -1,8 +1,8 @@
 # Developed by Mirko J. Rodríguez mirko.rodriguezm@gmail.com
 
-# ----------------------------
-# REST service under port 9000
-# ----------------------------
+# ------------------------
+# REST service via FastAPI
+# ------------------------
 
 from typing import Optional
 from fastapi import FastAPI
@@ -11,9 +11,9 @@ app = FastAPI()
 
 
 #Define a default route
-@app.route('/')
+@app.get('/')
 def main_page():
-    return '¡REST service is active via FastAPI (version: '+fastapi.__version__+')!'
+    return '¡REST service is active via FastAPI'
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
